4adf5b5f4187fca2b435fdec43b705a641ad37d0
JonathanAtsu <dataatsu2@gmail.com>
2025-09-01 18:50:30 -0400

    revison

---
+++ main.tf
@@ -0,0 +1,175 @@
+// vpc code 
+resource "aws_vpc" "vpc1" {
+  cidr_block = "172.120.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags ={
+    Name = "utc-app"
+    env = "Dev"
+    Team = "DevOps"
+  }
+}
+# Internet gateway
+
+resource "aws_internet_gateway" "igw1" {
+ vpc_id =  aws_vpc.vpc1.id
+ tags ={
+    Name = "utc-app"
+    env = "Dev"
+    Team = "DevOps"
+  }
+}
+# subenet public 
+
+resource "aws_subnet" "pusub1" {
+  vpc_id = aws_vpc.vpc1.id
+  map_public_ip_on_launch = true
+  cidr_block = "172.120.1.0/24"
+  availability_zone = "us-east-1a"
+  tags={
+    Name = "public-useast-1a"
+  }
+}
+resource "aws_subnet" "pusub2" {
+  vpc_id = aws_vpc.vpc1.id
+  map_public_ip_on_launch = true
+  cidr_block = "172.120.2.0/24"
+  availability_zone = "us-east-1b"
+  tags={
+    Name = "public-useast-1b"
+  }
+}
+// private subnets
+
+resource "aws_subnet" "privsub1" {
+  vpc_id = aws_vpc.vpc1.id
+  availability_zone = "us-east-1a"
+  cidr_block = "172.120.3.0/24"
+  tags={
+    Name ="private-us-east-1a"
+  }
+}
+resource "aws_subnet" "privsub2" {
+  vpc_id = aws_vpc.vpc1.id
+  availability_zone = "us-east-1b"
+  cidr_block = "172.120.4.0/24"
+  tags={
+    Name ="private-us-east-1b"
+  }
+}
+
+# Nat gateway
+
+
+resource "aws_eip" "eip1" {
+  
+}
+resource "aws_nat_gateway" "gtw1" {
+  subnet_id = aws_subnet.pusub1.id
+  allocation_id = aws_eip.eip1.id
+  tags= {
+    Name = "Nategateway"
+    env = "Dev"
+  }
+}
+
+
+# Private route table
+
+
+resource "aws_route_table" "rt1" {
+  vpc_id = aws_vpc.vpc1.id 
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.gtw1.id 
+  }
+}
+
+#public route table 
+
+resource "aws_route_table" "rt2" {
+  vpc_id = aws_vpc.vpc1.id 
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw1.id 
+  }
+}
+# private route table association
+
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id = aws_subnet.privsub1.id 
+  route_table_id = aws_route_table.rt1.id
+}
+resource "aws_route_table_association" "rta2" {
+  subnet_id = aws_subnet.privsub2.id 
+  route_table_id = aws_route_table.rt1.id
+}
+
+# public  route table association
+
+
+resource "aws_route_table_association" "rtpub1" {
+  subnet_id = aws_subnet.pusub1.id 
+  route_table_id = aws_route_table.rt2.id 
+}
+resource "aws_route_table_association" "rtpub2" {
+  subnet_id = aws_subnet.pusub2.id 
+  route_table_id = aws_route_table.rt2.id 
+}
+# backend.tf
+
+
+terraform {
+  backend "s3" {
+    bucket = "sk-w7-terraformstatebucket" # replace with your bucket
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+    #use_lockfile = true
+  }
+}
+# sg.tf
+
+
+resource "aws_security_group" "web_server_sg" {
+  name        = "web-server-sg"
+  description = "Allow web traffic and SSH"
+  vpc_id      = aws_vpc.vpc1.id 
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS from anywhere"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from specific IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["98.197.203.225/32"]  # Replace with your IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # All protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-server-security-group"
+  }
+  depends_on = [ aws_vpc.vpc1 ]
+}
